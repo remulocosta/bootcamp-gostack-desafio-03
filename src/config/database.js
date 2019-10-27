bc2f7d2d83667467ebb45293b6a4e2253bf4b523
Remulo Costa <remulo.costa@gmail.com>
2019-10-27 14:00:11 -0300

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+// commonjs syntax
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'racdelphi',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
