6fb4561e851d986f5edea6b8598eff00277a5387
Remulo Costa <remulo.costa@gmail.com>
2019-12-23 16:38:33 -0300

    correção UserController metodo update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -46,7 +46,7 @@ class UserController {
           oldPassword ? field.required() : field
         ),
       confirmPassword: Yup.string().when('password', (password, field) =>
-        password ? field.required().oneof([Yup.ref('password')]) : field
+        password ? field.required().oneOf([Yup.ref('password')]) : field
       ),
     });
 
